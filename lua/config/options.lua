21df85c25f301f68511cd4c2499d922ade0e3adb
Daniel Bulla <devel@danielbulla.de>
2025-02-19 13:16:41 +0100

    Add C++/Rust plugins (compiler, overseer, crates)

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.o.guifont = "JetBrainsMono Nerd Font:h10"
+vim.g.neovide_cursor_animate_command_line = true
+vim.g.neovide_cursor_animate_in_insert_mode = true
